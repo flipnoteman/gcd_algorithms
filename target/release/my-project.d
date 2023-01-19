5326a65bc59f57c9c538b8471868ec1519d70d79
Samuel <sammys.email.home@gmail.com>
2023-01-19 13:22:18 -0600

    Cleaned things up

--- target/release/my-project.d
+++ target/release/my-project.d
@@ -1,1 +1,1 @@
-C:\Users\sammy\CLionProjects\gcd_algorithms\target\release\my-project.exe: C:\Users\sammy\CLionProjects\gcd_algorithms\gcd\src\lib.rs C:\Users\sammy\CLionProjects\gcd_algorithms\src\main.rs C:\Users\sammy\CLionProjects\gcd_algorithms\tests\src\lib.rs
+C:\Users\sammy\CLionProjects\gcd_algorithms\target\release\my-project.exe: C:\Users\sammy\CLionProjects\gcd_algorithms\libraries\gcd\src\lib.rs C:\Users\sammy\CLionProjects\gcd_algorithms\libraries\tests\src\lib.rs C:\Users\sammy\CLionProjects\gcd_algorithms\src\main.rs
